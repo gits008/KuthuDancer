3e66ae96002f135f24b8055c7e2d2af592d1bd86
gits008 <57093027+gits008@users.noreply.github.com>
2019-10-28 17:07:04 +0530

    Add files via upload

---
+++ src/main/java/com/KuthuDancer/Simbu/App.java
@@ -0,0 +1,16 @@
+package com.KuthuDancer.Simbu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	for (int i=1;i<5;i++)
+    	{
+        System.out.println( "Hello World!" );
+    	}
+    }
+}
